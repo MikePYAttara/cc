aaa99177da5ace2f8516f8223b05ff94d371aef9
Mike Perry Y Attara <mpyebattara@gmail.com>
2018-06-30 20:11:00 +0000

    update sw location

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/cc/sw.js', { scope: '/cc/'})
   .then(reg => {
     // Registration successful
     console.log('Service Worker registered!')
--- sw.js
+++ sw.js
@@ -16,25 +16,25 @@ self.addEventListener('install', event => {
   const request = window.indexedDB.open(dbName, 1);
   event.waitUntil(
     // On success add data
-    db.onsuccess = event => {
+    request.onsuccess = event => {
       // Query for data
       const query = event.target.result;
 
       // Check if data exist in Database
-      const currency = query.transaction('currencies').objectStore('currencies').get(data.symbol);
+      const currency = query.transaction('currencies').objectStore('currencies').get(data.id);
 
       currency.onsuccess = event => {
           const dbData = event.target.result;
           const store = query.transaction('currencies', 'readwrite').objectStore('currencies');
           if (!dbData) {
               // Save data
-              store.add(data, data.symbol);
+              store.add(data, data.id);
           } else {
               //Update data
-              store.put(data, data.symbol);
+              store.put(data, data.id);
           };
       }
-  } 
+    }; 
   );
 });
 
