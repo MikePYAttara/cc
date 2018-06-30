85089ec1685bdb40d3cf7e234d5f37529f62a50a
Mike Perry Y Attara <mpyebattara@gmail.com>
2018-06-30 18:40:19 +0000

    Complete conversion

--- js/script.js
+++ js/script.js
@@ -27,44 +27,38 @@ let currencyInnerHTML = "";
 const url = 'https://free.currencyconverterapi.com/api/v5/currencies';
 fetch(url)
 .then(res => res.json())
-.then(res => {
-  const currencies = res.results;
+.then(data => {
+  const currencies = data.results;
   for (let currency in currencies) {
-    currencyInnerHTML += `<option value=${currency.id}>${currency.id}</option>`
+    currencyInnerHTML += `<option value=${currency}>${currency}</option>`;
   };
   document.querySelector('#from-currency').innerHTML =`<option value="">Select Currency</option>` + currencyInnerHTML;
   document.querySelector('#to-currency').innerHTML =`<option value="">Select Currency</option>` + currencyInnerHTML;
 })
 
 // Select button
-document.querySelector('#convert').on('click', convertCurrency());
+document.querySelector('#convert').addEventListener('click', convertCurrency);
 
 // Conversion process
 function convertCurrency() {
+  console.log('convertCurrency() called')
   const fc = document.querySelector('#from-currency');
   const tc = document.querySelector('#to-currency');
   const fromCurrency = fc.options[fc.selectedIndex].value;
   const toCurrency = tc.options[tc.selectedIndex].value;
   const query = `${fromCurrency}_${toCurrency}`;
   const amt = parseFloat(document.querySelector('#amount').value);
-  const url = `https://www.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+  const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
   fetch(url)
-  .then(res => {
-    let body = "";
-    res.on('data', chunk => {
-      body += chunk;
-    });
-    res.on('end', () => {
-      const jsonObj = JSON.parse(body);
-      const val = jsonObj[query];
-      if (val) {
-        const total = val * amt;
-        Math.round(total * 100) / 100;
-      } else {
-        const err = new Error(`Value not found for ${query}`);
-        console.log(err);
-      };
-    });
+  .then(res => res.json())
+  .then(data => {
+    const val = data[query];
+    if (val) {
+      let total = parseFloat(val) * amt;
+      total = Math.round(total * 100) / 100;
+      // document.querySelector('#output').innerHTML = total;
+      console.log(total);
+    }
   })
   .catch(err => console.log(err))
 }
